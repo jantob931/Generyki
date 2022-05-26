6684063f18b76fd62fe6e6b9624159c8bcd4212b
Jan Tobiczyk <j.tobiczyk42@gmail.com>
2022-05-26 21:52:37 +0200

    Added functions

---
+++ Generyki1/DataProviders/Extensions/CarsHelper.cs
@@ -0,0 +1,17 @@
+﻿using Generyki1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generyki1.DataProviders.Extensions
+{
+    public static class CarsHelper
+    {
+    public static IEnumerable<Car> ByColor(this IEnumerable<Car> query,string color)
+        {
+            return query.Where(c => c.Color == color);
+        }
+    }
+}
